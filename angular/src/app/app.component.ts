8149639a5de489ad79b8adbc18d6220c0c6dfc06
habiburrahman-mu <habiburrahman-mu@users.noreply.github.com>
2024-06-16 11:43:47 +0600

    added input to submit user bid

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HttpService } from './services/http.service';
 import { Observable, catchError, map, of, startWith } from 'rxjs';
 import Auction from './models/Auction';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -17,12 +18,27 @@ export class AppComponent implements OnInit {
 
   title = 'SignalR';
   list$ = this.httpService.getAllAuctions().pipe(
-    map(x => ({loading: false, data: x, error: false})),
-    startWith({loading: true, data: undefined, error: false}),
-    catchError(err => of({loading: false, data: undefined, error: true}))
+    map(data => (
+      {
+        loading: false,
+        data: data.map(x => ({...x, newBid: x.currentBid + 1} as AuctionExtended)),
+        error: false
+      })),
+    startWith({ loading: true, data: undefined, error: false }),
+    catchError(err => of({ loading: false, data: undefined, error: true }))
   );
 
 
   ngOnInit(): void {
   }
+
+  onClickBid(data: AuctionExtended) {
+    console.log('pressed for bid');
+    console.table(data);
+  }
 }
+
+interface AuctionExtended extends Auction {
+  newBid: number;
+}
+
