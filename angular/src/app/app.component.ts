2e250fbaf79c3c71a4dbac4ba060c6fbc2d854a1
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2024-07-13 13:09:21 +0600

    ⏫ updated bid list and added post bid functionalities

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpService } from './services/http.service';
 import { Observable, catchError, map, of, startWith } from 'rxjs';
 import Auction from './models/Auction';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
@@ -15,13 +16,16 @@ import { FormsModule } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   private httpService = inject(HttpService);
+  private destroyRef = inject(DestroyRef);
 
   title = 'SignalR';
+  bidId = signal(0);
+  isLoading = signal(false);
   list$ = this.httpService.getAllAuctions().pipe(
     map(data => (
       {
         loading: false,
-        data: data.map(x => ({...x, newBid: x.currentBid + 1} as AuctionExtended)),
+        data: data.map(x => ({ ...x, newBid: x.currentBid + 1 } as AuctionExtended)),
         error: false
       })),
     startWith({ loading: true, data: undefined, error: false }),
@@ -32,8 +36,18 @@ export class AppComponent implements OnInit {
   }
 
   onClickBid(data: AuctionExtended) {
-    console.log('pressed for bid');
-    console.table(data);
+    this.bidId.set(data.id);
+    this.isLoading.set(true);
+    this.httpService.placeBid(data.id, data.newBid)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: _ => {
+          this.isLoading.set(false);
+        },
+        error: err => {
+          this.isLoading.set(false);
+        }
+      });
   }
 }
 
